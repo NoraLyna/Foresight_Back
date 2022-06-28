6cf9a727b2b119f6a23b22d4246d7af2d5f7d0a1
theBentspoon <103569804+theBentspoon@users.noreply.github.com>
2022-06-28 20:10:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser')
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const db = require('./api')
 
 
