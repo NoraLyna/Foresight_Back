e298bcf1401c671af57128798f88d7d77f225539
theBentspoon <103569804+theBentspoon@users.noreply.github.com>
2022-06-28 22:06:48 +0100

    Update connection.js

--- connection.js
+++ connection.js
@@ -8,11 +8,20 @@ const { Pool, Client } = require('pg')
   password: 'openpgpwd',
   port: 5432,
 })*/
-const isProduction = process.env.NODE_ENV === "production";
+
+const pool = new Pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+})
+
+/*const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-});
+});*/
 
 module.exports = {
     pool,
