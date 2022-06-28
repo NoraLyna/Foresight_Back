d8e30128023f66992dfc6651ba573b219304ae86
theBentspoon <103569804+theBentspoon@users.noreply.github.com>
2022-06-28 16:55:45 +0100

    Update api.js

--- api.js
+++ api.js
@@ -67,10 +67,11 @@ const getOperationInfoByState = (request, response) => {
 
 
     var qry = 
-    `SELECT * FROM operation_transport opt, transporteurs tp, assures asr  
+    `SELECT * FROM operation_transport opt, transporteurs tp, assures asr, vehicules veh 
     WHERE opt.transporteur_id = tp.id_transporteur 
     AND opt.assure_id = asr.id_assure 
     AND etat_operation = $1 
+    AND veh.id_vehicule = opt.vehicule_id
     ORDER BY opt.id_operation`;
 
     pool.query(qry,[etat_op], (err, res) => {
