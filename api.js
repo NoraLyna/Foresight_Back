f7c9860072df1fff5b8f121f9f90a99eb0c84fac
theBentspoon <103569804+theBentspoon@users.noreply.github.com>
2022-06-29 21:07:57 +0100

    Update api.js

--- api.js
+++ api.js
@@ -90,7 +90,7 @@ const getMontantRemboursement = (request, response) =>{
         else
         {
             console.log(res.rows);
-            var d_parcourue = parseFloat(res.rows[0]['distance_realise']);
+            var d_parcourue = parseFloat(res.rows[0]['kilometrage_realise']);
             var d_operateur = parseFloat(res.rows[0]['dist_ass_op']);
             var tmp_att = parseFloat(res.rows[0]['temps_attente']);
             var type_trans = res.rows[0]['type_vehicule'];
