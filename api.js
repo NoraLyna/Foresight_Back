132c5b6bf44ca4206528adc2a8f29a1069850170
theBentspoon <103569804+theBentspoon@users.noreply.github.com>
2022-06-29 09:49:03 +0100

    adding login operateur

--- api.js
+++ api.js
@@ -501,6 +501,7 @@ module.exports = {
     getTransporterOperationsByState,
 
     getOperateurs,
+    loginOperateur,
     getOperateursTransporters,
     getOperateursOperations,
     getOperateurOperationsStats,
